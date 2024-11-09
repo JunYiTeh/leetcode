15bbd1d4786b98ff9fe54e222d538094dc704705
Jun Yi Teh <68840157+JunYiTeh@users.noreply.github.com>
2024-11-09 23:48:55 +1030

    leetcode 217

---
+++ 217-contains-duplicate.cpp
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/contains-duplicate/
+class Solution {
+    public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> list;
+        
+        for (int i=0; i<nums.size(); i++) {
+            list[nums[i]] = list[nums[i]] + 1;
+            if (list[nums[i]] > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+/*
+given int array nums, return true if value appears at least twice in array, and return false if every element distinct
+nums = [1, 2, 3, 1]
+list = [1, 2, 3]
+returns true
+
+nums = [1, 2, 3, 3, 4]
+returns true
+
+i = 0, current element = 1
+
+*/
+
+// Works but time limit exceeded
+/*
+class Solution {
+    public:
+    bool containsDuplicate(vector<int>& nums) {
+        for (int i=0; i<nums.size(); i++) {
+            for (int j=0; j<nums.size(); j++) {
+                if (i != j && nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+*/
